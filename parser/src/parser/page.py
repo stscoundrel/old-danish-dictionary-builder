e23b6c43b7567cbd198f64b2c6804be1092c57a0
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2023-11-18 16:08:54 +0200

    Add simple entry parsing
    Naive approach, which leaves most edge cases unsolved

--- parser/src/parser/page.py
+++ parser/src/parser/page.py
@@ -1,3 +1,24 @@
+import re
+from typing import NamedTuple
+
+
+class Entry(NamedTuple):
+    headword: str
+    definitions: str
+
+    @staticmethod
+    def from_raw_entry(raw_entry: str) -> "Entry":
+        # Naive expectation: first word is headword.
+        # TODO: GH-16 better detection.
+        parts = raw_entry.split(" ", maxsplit=1)
+
+        return Entry(
+            headword=parts[0],
+            # TODO: GH-17 clean up content.
+            definitions=parts[1],
+        )
+
+
 class Page:
     _meta_parts: list[str] | None = None
 
@@ -15,8 +36,15 @@ class Page:
 
         return self._meta_parts
 
+    def get_separators_for(self, letter: str) -> list[str]:
+        return [f"— {letter}", f"   {letter}"]
+
     def get_entry_separators(self) -> set[str]:
-        return {f"— {letter}" for letter in self.get_letters_in_page()}
+        return {
+            separator
+            for letter in self.get_letters_in_page()
+            for separator in self.get_separators_for(letter)
+        }
 
     def is_left_side_page(self) -> bool:
         return self._get_meta_parts()[0].isnumeric()
@@ -40,3 +68,29 @@ class Page:
             letters.add(self._get_meta_parts()[1][0].upper())
 
         return letters
+
+    def get_entries(self) -> list[Entry]:
+        raw_entries = ["\n".join(self.content)]
+
+        for letter in self.get_letters_in_page():
+            # TODO: GH-14 may need additional separators, perhaps based on
+            # the end of previous definition.
+            separators_regex = "|".join(self.get_separators_for(letter))
+
+            # Unsplit content should always be in the last entry.
+            entries_for_letter = re.split(separators_regex, raw_entries[-1])
+
+            # TODO: GH-13. Only append if not the first entry.
+            # If first entry, detect if start of entry or not.
+            # Probably needs to support incomplete entries to be patched later.
+
+            # Append base letter back
+            entries_for_letter = [f"{letter}{entry}" for entry in entries_for_letter]
+
+            raw_entries = raw_entries[0:-2] + entries_for_letter
+
+        # Format string entries to structures.
+        # TODO: GH-16 recognize incorrect headwords, append to previous entries.
+        entries = [Entry.from_raw_entry(raw_entry) for raw_entry in raw_entries]
+
+        return entries
