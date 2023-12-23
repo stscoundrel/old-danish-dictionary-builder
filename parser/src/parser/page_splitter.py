2672d67ecbd40992e3977ccd3cd4e78d2b6f1968
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2023-12-23 17:15:20 +0200

    Page splitter: add full mapping of pages to split
    Also adds preliminary mystery letter handling, should first part of the page not have any recognizable entries.

--- parser/src/parser/page_splitter.py
+++ parser/src/parser/page_splitter.py
@@ -3,9 +3,28 @@ from typing import Final
 from src.parser.columns import parse_column
 from src.parser.page import Page
 
+# Pages that contain more than one letter should be handled as two pages.
 # Filenames of pages to be split & line index where split should happen.
 LETTER_SPLIT_MAPPING: Final[dict[str, int]] = {
     "87-axelkøbstad.txt": 50,
+    "329-bøs.txt": 47,
+    "484-fabel.txt": 4,
+    "962-gørrel.txt": 37,
+    "1200-høved(s)mand.txt": 24,
+    "1269-ivæve.txt": 12,
+    "1300-jødetempel.txt": 30,
+    "1552-køterkro.txt": 7,
+    "1912-mørsk.txt": 21,
+    "2005-nøvelige.txt": 23,
+    "2172-øxentorv.txt": 18,
+    "2639-røtte (rotte).txt": 38,
+    "3173-søstergård.txt": 26,
+    "3408-tøve.txt": 16,
+    "3806-vævel.txt": 49,
+    "3807-ybisk.txt": 12,
+    "3839-æven[æm]tyrlig.txt": 25,  # TODO: GH-37
+    "3887-årtrålig.txt": 37,
+    "3912-æven[æm]tyrlig.txt": 25,  # TODO: GH-37
 }
 
 
@@ -23,14 +42,19 @@ class PageSplitter:
 
         # Split by the split point, but append meta line to the second part too.
         combined_column_1_lines = parse_column(lines[0:split_point])
-        combined_column_2_lines = parse_column(
-            [lines[0]] + lines[split_point + 1 :]  # noqa: E203
-        )
-
+        combined_column_2_lines = parse_column([lines[0]] + lines[split_point:])
         page1, page2 = (Page(combined_column_1_lines), Page(combined_column_2_lines))
 
         # We expect pages to have two letters maximum.
         letters = page1.get_letters_in_page()
+        assert len(letters) <= 2
+
+        # Should there only be one letter, the metaline is incorrect. For example, page E - F.
+        # In that case, it is safe enough to expect that the first is incorrect & should be the
+        # previous letter of the alphabet. May later need custom mapping if more complex cases appear.
+        if len(letters) == 1:
+            letters = [cls._get_previous_letter_of_dictionary(letters[0]), letters[0]]
+
         assert len(letters) == 2
 
         # Inject letters to correct pages, indicating both are not available in both.
@@ -38,3 +62,12 @@ class PageSplitter:
         page2.set_letters_in_page([letters[1]])
 
         return page1, page2
+
+    @staticmethod
+    def _get_previous_letter_of_dictionary(letter: str) -> str:
+        # TODO: actual logic if needed. Currently only covers
+        # special-case-by-special-case.
+        if letter == "F":
+            return "E"
+
+        raise NotImplementedError(f"No handling for letter {letter}!")
--- parser/tests/test_page_splitter.py
+++ parser/tests/test_page_splitter.py
@@ -1,3 +1,4 @@
+from src.parser.entry import EntryStatus
 from src.parser.page_splitter import PageSplitter
 from tests import open_test_file
 
@@ -75,3 +76,45 @@ def test_splits_page_correctly() -> None:
     )
 
     assert a_entries[3].definitions == expected_content
+
+
+def test_splits_page_of_unknown_first_letter_correctly() -> None:
+    """
+    Background: page e-to-f has no full entries for E,
+    so letter has to be deduced separately.
+    """
+
+    e_to_f = open_test_file("split-e-to-f.txt")
+
+    page_e, page_f = PageSplitter.split_page("484-fabel.txt", e_to_f)
+
+    # Page side should be shared between the split siblings.
+    assert page_e.is_right_side_page() is True
+    assert page_f.is_right_side_page() is True
+
+    # Pages should have separated available letters.
+    assert page_e.get_letters_in_page() == ["E"]
+    assert page_f.get_letters_in_page() == ["F"]
+
+    # Pages should have expected amounts of entries.
+    e_entries = page_e.get_entries()
+    f_entries = page_f.get_entries()
+    assert len(e_entries) == 1
+    assert len(f_entries) == 5
+
+    # The E-page should have incomplete entry.
+    assert e_entries[0].status == EntryStatus.PART_OF_PREVIOUS_ENTRY
+    assert (
+        e_entries[0].headword == "hawe"
+    )  # Not an actual headword, as stated by status.
+
+    assert e_entries[0].definitions == (
+        "olden swin oc haleff olden-gaffwe aff skoflwen oc gester), som aff andre hans thener oc en skeligh eytte "
+        "om areth, sagefaldh oc brøde (1493). D. Mag. IV. 12. Vistnok = ejgt (9: ægt), se d. 0."
+    )
+
+    expected_f_headwords = ["Fabel", "Fabelhøne,.no.", "Fabel", "Fabian", "Fad"]
+
+    assert [entry.headword for entry in f_entries] == expected_f_headwords
+
+    assert f_entries[1].definitions == "= fabelhans (om kvinder). Moth. Smlgn. byhøne."
