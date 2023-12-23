03d243027e7c5b25a91eea796fc8d896744b0392
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2023-12-23 21:36:12 +0200

    Columns: if no separators detected, split in middle

--- parser/src/parser/columns.py
+++ parser/src/parser/columns.py
@@ -1,33 +1,51 @@
 from typing import Final
 
-_divider: Final[str] = "|"
+_vertical_divider: Final[str] = "|"
+_exlamation_divider: Final[str] = " ! "
 _spaces_divider: Final[str] = "   "
 
 
-def _get_column_divider(line: str) -> str:
-    if _divider in line:
+def _get_column_divider(line: str) -> str | None:
+    if _vertical_divider in line:
         # Divider was read in OCR, simple case.
-        return _divider
+        return _vertical_divider
+
+    if _exlamation_divider in line:
+        # Occasionally vertical line is read as exlamation mark.
+        return _exlamation_divider
 
     # If divider is missing, it was probably misread.
     # It generally means there will be a
     # grouping of spaces where the divider should be.
     # Four spaces seems common enough case to act as divider.
-    return _spaces_divider
+    if _spaces_divider in line:
+        return _spaces_divider
+
+    # No clear divider found, other handling needed.
+    return None
 
 
 def _get_divided_lines(line: str) -> list[str]:
     divider = _get_column_divider(line)
-    divided = line.split(divider, 1)
 
-    # If we're dealing with spaces divider, we want to preserve
-    # the whitespace after the split. Therefore, append it to
-    # all but the first item in split list.
-    if divider == _spaces_divider:
-        for idx, divided_part in enumerate(divided[1:]):
-            divided[idx + 1] = f"{_spaces_divider}{divided_part}"
+    if divider:
+        divided = line.split(divider, 1)
 
-    return divided
+        # If we're dealing with spaces divider, we want to preserve
+        # the whitespace after the split. Therefore, append it to
+        # all but the first item in split list.
+        if divider == _spaces_divider:
+            for idx, divided_part in enumerate(divided[1:]):
+                divided[idx + 1] = f"{_spaces_divider}{divided_part}"
+
+        return divided
+
+    # No clear divider available. Most likely issue of OCR not reading it correctly.
+    # Our best bet: divide it in the middle. Sometimes there are two spaces there,
+    # but there are more exotic scenarios too. For now, lets see how far we get with
+    # just division down the middle. Based on small sample size it is accurate enough
+    # not to break entries, but will cause some typos along the way.
+    return [line[: len(line) // 2], line[len(line) // 2 :]]  # noqa: E203
 
 
 def parse_column(page: list[str]) -> list[str]:
@@ -35,20 +53,21 @@ def parse_column(page: list[str]) -> list[str]:
     right_column = []
 
     for line in page[1:]:  # First line is meta info.
-        divided = _get_divided_lines(line)
+        if len(line) > 20:  # Skip letter headings and oddities.
+            divided = _get_divided_lines(line)
 
-        match len(divided):
-            case 0:
-                # Empty line
-                continue
-            case 1:
-                left_column.append(line)
-            case 2:
-                left_column.append(divided[0])
-                right_column.append(divided[1])
-            case _:
-                print("Unexpected split!")
-                print(line)
+            match len(divided):
+                case 0:
+                    # Empty line
+                    continue
+                case 1:
+                    left_column.append(line)
+                case 2:
+                    left_column.append(divided[0])
+                    right_column.append(divided[1])
+                case _:
+                    print("Unexpected split!")
+                    print(line)
 
     return [page[0]] + left_column + right_column
 
