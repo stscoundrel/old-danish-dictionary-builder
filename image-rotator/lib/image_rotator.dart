48694b919a382a806cafcd09e235250defc13ffe
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-01-01 16:33:59 +0200

    Add image rotator script
    Rotates problematic pages whose original scan is skewed. Based on filenames to degrees map & run in Dart.

---
+++ image-rotator/lib/image_rotator.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+import 'package:image/image.dart' as img;
+
+Future<void> rotateImage(
+    String inputFilePath, String outputFilePath, double rotationDegrees) async {
+  try {
+    // Read the input image
+    final fileBytes = await File(inputFilePath).readAsBytes();
+    final image = img.decodeGif(fileBytes);
+
+    if (image != null) {
+      // Rotate the image
+      final rotatedImage = img.copyRotate(image, rotationDegrees);
+
+      // Save the rotated image to a new file
+      await File(outputFilePath).writeAsBytes(img.encodeGif(rotatedImage));
+
+      print(
+          'Image rotation completed. Check the output file at: $outputFilePath');
+    } else {
+      print('Failed to decode the input image.');
+    }
+  } catch (e) {
+    print('Error: $e');
+  }
+}
