1954aa74ecc22e5c42c2f0a45cebe1b7c42d0773
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-01-06 16:26:44 +0200

    Handle re-OCR'd pages in parser.
    Closes #57

--- parser/src/parser/meta_line.py
+++ parser/src/parser/meta_line.py
@@ -2,10 +2,11 @@ from typing import Final
 
 PAGES_TO_IRREGULAR_META_LINE_INDEXES: Final[dict[str, int]] = {
     "71-arbejdelse.txt": 1,
+    "97-balstyrig.txt": 1,
     "430-eftergøre.txt": 1,
     "875-gildeskorn.txt": 1,
     "897-gnistne.txt": 1,
-    "1109-hosskrift.txt": 1,  # TODO: GH-57 could use a new OCR.
+    "1109-hosskrift.txt": 1,
     "1138-husbrand.txt": 1,
     "1233-indermere (inderst).txt": 2,
     "1267-istædelæder.txt": 1,
