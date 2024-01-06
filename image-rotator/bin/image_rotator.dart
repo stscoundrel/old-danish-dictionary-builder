6e9f898dc013fda93431861eed188d85c5195107
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-01-06 16:25:51 +0200

    Image rotator: adjust balstyrig rotation
    Seems lines are really tricky to match in this one.

--- image-rotator/bin/image_rotator.dart
+++ image-rotator/bin/image_rotator.dart
@@ -1,7 +1,7 @@
 import 'package:image_rotator/image_rotator.dart' as image_rotator;
 
 const Map<String, double> imagesToRotate = {
-  'resources/images/97-balstyrig.gif': 0.5,
+  'resources/images/97-balstyrig.gif': 1.0,
   'resources/images/1109-hosskrift.gif': 1.15,
 };
 
