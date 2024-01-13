657056c36f1d2978b6aefc752ee38470d8dd42fd
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-01-13 18:26:01 +0200

    Skewed pages: rotate "timesand"

--- image-rotator/bin/image_rotator.dart
+++ image-rotator/bin/image_rotator.dart
@@ -4,6 +4,7 @@ const Map<String, double> imagesToRotate = {
   'resources/images/97-balstyrig.gif': 1.0,
   'resources/images/1109-hosskrift.gif': 1.15,
   'resources/images/2387-røtte (rotte).gif': -2.33,
+  'resources/images/3021-timesand.gif': 1.1,
 };
 
 void main(List<String> arguments) {
