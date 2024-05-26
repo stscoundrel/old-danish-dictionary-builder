c81fa64943972ef44431b84219978442adc88c1a
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-05-26 21:11:16 +0300

    Rotate 2-adamsbid
    Closes #122
    Closes #121

--- image-rotator/bin/image_rotator.dart
+++ image-rotator/bin/image_rotator.dart
@@ -1,6 +1,7 @@
 import 'package:image_rotator/image_rotator.dart' as image_rotator;
 
 const Map<String, double> imagesToRotate = {
+  'resources/images/2-adamsbid.gif': -0.4,
   'resources/images/97-balstyrig.gif': 1.0,
   'resources/images/1000-S. Hansbrod.gif': -1.4,
   'resources/images/1109-hosskrift.gif': 1.15,
