e08f5f6ee8ee65adb1d578f58ab0752d0bda1204
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-06-01 20:10:00 +0300

    Rotate "midsunds", apply exceptions after new OCR
    Closes #128

--- image-rotator/bin/image_rotator.dart
+++ image-rotator/bin/image_rotator.dart
@@ -5,6 +5,7 @@ const Map<String, double> imagesToRotate = {
   'resources/images/97-balstyrig.gif': 1.0,
   'resources/images/1000-S. Hansbrod.gif': -1.4,
   'resources/images/1109-hosskrift.gif': 1.15,
+  'resources/images/1783-midsunds.gif': -1.9,
   'resources/images/1817-metal.gif': -0.5,
   'resources/images/2387-røtte (rotte).gif': -2.33,
   'resources/images/3021-timesand.gif': 1.1,
